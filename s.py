14a9bb09b487eda24264dc8c34c7a9b580a6cc96
Krish Thukral <krish.23fe10cse00679@muj.manipal.edu>
2026-03-31 15:39:16 +0530

    Update s.py

--- s.py
+++ s.py
@@ -238,7 +238,7 @@ def get_gemini_explanation(prediction, shap_df):
         {feature_summary}
         Write a short encouraging summary under 80 words with one practical tip.
         """
-        genai.configure(api_key="AIzaSyA_DkN3Z5O717YLWYIXnzFJwqRPo3CxCW8")
+        genai.configure(api_key="AIzaSyDtwQVrR6JE3eKmAAYxYnCYLnNb_JD3qPk")
         gemini_model = genai.GenerativeModel("gemini-2.5-flash")
         return gemini_model.generate_content(prompt).text
 
